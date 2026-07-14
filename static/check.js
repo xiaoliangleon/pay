69b8fed921c609385909e97b0a253b60c098539a
leon <88283857+xiaoliangleon@users.noreply.github.com>
2026-07-14 22:17:00 +0800

    Delete static directory

--- static/check.js
+++
@@ -1,9 +0,0 @@
-//判断是否是非法访问
-wechat = "MicroMessenger";
-alipay = "AlipayClient";
-qqBrowser = "MQQBrowser"
-userAgent = navigator.userAgent;
-
-if (userAgent.indexOf(wechat)==-1 && userAgent.indexOf(alipay)==-1 && userAgent.indexOf(qqBrowser)==-1){
-  window.location.href = "/index.html"
-}
